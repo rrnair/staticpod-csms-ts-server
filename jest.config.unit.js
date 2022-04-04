046d747e11c8d9914b9fd7996259ba97f6195863
Ratheesh Nair <ratheesh.ravi.nair@gmail.com>
2022-04-04 23:49:32 +0530

    Initial version

---
+++ jest.config.unit.js
@@ -0,0 +1,14 @@
+module.exports = {
+  clearMocks: false,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  testEnvironment: 'node',
+  roots: ['<rootDir>/test'],
+  testMatch: [
+    '**/*.test.ts'
+  ],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  }
+};
